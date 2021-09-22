550cb8865765b9375c1dfbbb610b10bbfa7df3bd
shrushti2000 <shrushtipolekar@gmail.com>
2021-09-22 21:05:39 +0530

    feat: type-ahead feature

---
+++ type-ahead/index.js
@@ -0,0 +1,37 @@
+const url='https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+var cities=[]
+fetch(url)
+.then(res=>res.json())
+.then(data=>cities.push(...data))
+
+
+function findMatches(wordToMatch){
+    return cities.filter(place=>{
+        const regex=new RegExp(wordToMatch,'gi')
+        return place.city.match(regex) || place.state.match(regex)
+    })
+}
+
+function displayMatches(){
+    const matchArray=findMatches(this.value,cities);
+    const html=matchArray.map(place=>{
+        const regex=new RegExp(this.value,'gi')
+        const cityName=place.city.replace(regex,`<span class="h1">${this.value}</span>`)
+        const stateName=place.state.replace(regex,`<span class="h1">${this.value}</span>`)
+        return `
+        <li>
+        <span class="name">${cityName},${stateName}</span>
+        <span class="population">${this.value}</span>
+        </li>
+        `;
+    }).join('');
+    suggestions.innerHTML=html
+}
+
+const textInput=document.querySelector(".search")
+const suggestions=document.querySelector('.suggestions')
+
+textInput.addEventListener('change',displayMatches)
+textInput.addEventListener('keyup',displayMatches)
+
