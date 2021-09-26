583d3d908b1a4e2c42d0585843a27aad6e6a93b2
shrushti2000 <shrushtipolekar@gmail.com>
2021-09-26 13:23:03 +0530

    feat: parent child Selectors

---
+++ parent-child-Selectors/index.js
@@ -0,0 +1,6 @@
+var grandParent=document.querySelector('.grand-parent-container')
+var parent=document.querySelector('.parent-container')
+var child=document.querySelector('.child-container')
+
+parent.style.backgroundColor='grey' 
+console.log(grandParent.children)
